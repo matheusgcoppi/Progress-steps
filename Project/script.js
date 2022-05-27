15bc32ea8bde7773eeb82730c674b68b2e454a4f
matheusgcoppi <matheuscoppi22@gmail.com>
2022-05-27 12:08:04 -0300

    Update

---
+++ Project/script.js
@@ -0,0 +1,11 @@
+const progress = document.getElementById('progress')
+const preview = document.getElementById('preview')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle') //All because we have more than 1 class circle
+
+let currentActive = 1
+
+next.addEventListener('click', function(){
+    currentActive
+
+})
