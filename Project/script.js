19e964dffd66835da5b7af69e750e9b30633e985
matheusgcoppi <matheuscoppi22@gmail.com>
2022-05-30 21:58:46 -0300

    final update

--- Project/script.js
+++ Project/script.js
@@ -5,7 +5,43 @@ const circles = document.querySelectorAll('.circle') //All because we have more 
 
 let currentActive = 1
 
-next.addEventListener('click', function(){
-    currentActive
+next.addEventListener('click', () => {
+    currentActive++
 
+    if(currentActive > circles.length) {
+        currentActive = circles.length }
+
+        update()
 })
+
+preview.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1 }
+
+        update()
+})
+
+function update () {
+    circles.forEach((circle, index) => {
+        if(index < currentActive) {
+            circle.classList.add('active') }
+        else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        preview.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        preview.disabled = false
+        next.disabled = false
+    }
+}
